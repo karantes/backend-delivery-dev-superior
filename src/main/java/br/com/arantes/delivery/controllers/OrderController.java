baa1f8f385a7bc6659352fd42c0a24f770865c68
kaiqu <kaiqu@host.docker.internal>
2021-01-08 10:46:58 -0300

    POST /orders

--- src/main/java/br/com/arantes/delivery/controllers/OrderController.java
+++ src/main/java/br/com/arantes/delivery/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package br.com.arantes.delivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.arantes.delivery.dto.OrderDTO;
 import br.com.arantes.delivery.services.OrderService;
@@ -21,5 +25,12 @@ public class OrderController {
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findPedidosPendentes() {
 		return ResponseEntity.ok().body(orderService.findPedidosPendentes());
+	}
+
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = orderService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- src/main/java/br/com/arantes/delivery/services/OrderService.java
+++ src/main/java/br/com/arantes/delivery/services/OrderService.java
@@ -1,13 +1,20 @@
 package br.com.arantes.delivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.arantes.delivery.dto.OrderDTO;
+import br.com.arantes.delivery.dto.ProductDTO;
+import br.com.arantes.delivery.entity.Order;
+import br.com.arantes.delivery.entity.OrderStatus;
+import br.com.arantes.delivery.entity.Product;
 import br.com.arantes.delivery.repositories.OrderRepository;
+import br.com.arantes.delivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -15,7 +22,23 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
 	public List<OrderDTO> findPedidosPendentes() {
 		return orderRepository.findPedidosPendentes().stream().map(o -> new OrderDTO(o)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+
+		return new OrderDTO(orderRepository.save(order));
 	}
 }
